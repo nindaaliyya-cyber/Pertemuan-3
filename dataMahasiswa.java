ccf2123de9e01db6229bcd7783d76dac7d1abd55
nindaaliyya-cyber <nindaaliyya@gmail.com>
2026-02-19 13:20:41 +0700

    Add DataMahasiswa class to display student data

---
+++ dataMahasiswa.java
@@ -0,0 +1,23 @@
+public class DataMahasiswa {
+public static void main(String[] args) {
+
+    // Tipe data
+    long nim = 2301010001L;
+    String nama = "Budi Santoso";
+    char kelas = 'A';
+    boolean statusAktif = true;
+    double ipkSemester = 3.75;
+    int totalSks = 72;
+
+    // Output sesuai tampilan gambar
+    System.out.println("=== DATA MAHASISWA ===");
+    System.out.println("NIM           : " + nim);
+    System.out.println("Nama          : " + nama);
+    System.out.println("Kelas         : " + kelas);
+    System.out.println("Status Aktif  : " + statusAktif);
+    System.out.println("IPK Semester  : " + ipkSemester);
+    System.out.println("Total SKS     : " + totalSks);
+}
+
+
+}
